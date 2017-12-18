f3749a56fba88b06464872e070e7b90db6f0ec02
aswietlik <swietlik.aga123@gmail.com>
2017-12-18 10:54:24 +0100

    Temperature and liquid level sensors

---
+++ DS18B20.cpp
@@ -0,0 +1,85 @@
+#include "DS18B20.h"
+#include "DS1Wire.h"
+#include "mbed.h"
+#include <stdint.h>
+
+// Device byte commands over 1-wire serial
+enum COMMANDS { READ_ROM = 0x33, CONVERT = 0x44, READ_SCRATCHPAD = 0xBE,  SKIP_ROM = 0xCC };
+
+// device onboard register layout
+typedef struct {
+    uint8_t    LSB;
+    uint8_t    MSB;
+    uint8_t    Th;
+    uint8_t    Tl;
+    uint8_t    config;
+    uint8_t    reserved0xFF;
+    uint8_t    reserved0xCH;
+    uint8_t    reserved0x10;
+    uint8_t    Crc;
+} ScratchPad_t;
+
+
+DigitalOut conversionInProgress(LED4);  // conversion in progress
+DigitalOut resetFailure(LED1);          // for error reporting
+extern DigitalInOut sensorTemp;     // sensorTemp pin
+
+static void inError() {
+    while (1) {
+        resetFailure = !resetFailure;
+        wait(0.2);
+    }
+}
+
+void DoConversion() {
+    if (Reset(sensorTemp) != 0) {
+        inError();
+    } else {
+        conversionInProgress = 1;       // led on
+        WriteByte(sensorTemp, SKIP_ROM);            // Skip ROM
+        WriteByte(sensorTemp, CONVERT);             // Convert
+        while (ReadBit(sensorTemp) == 0) {
+            // wait for conversion to complete
+        }
+        conversionInProgress = 0;       // led off
+    }
+}
+
+uint32_t GetTemperature() {
+    uint32_t result = 0;
+    if (Reset(sensorTemp) != 0) {
+        inError();
+    } else {
+        ScratchPad_t scratchpad;
+        WriteByte(sensorTemp, SKIP_ROM);    // Skip ROM
+        WriteByte(sensorTemp, READ_SCRATCHPAD);    // Read Scrachpad
+        scratchpad.LSB = ReadByte(sensorTemp);
+        scratchpad.MSB = ReadByte(sensorTemp);
+        Reset(sensorTemp);    // terminate read as we only want temperature
+        result = ((scratchpad.MSB << 8) | scratchpad.LSB);
+    }
+    return result;
+}
+
+ROM_Code_t ReadROM() {
+    ROM_Code_t ROM_Code;
+    if (Reset(sensorTemp) != 0) {
+        inError();
+    } else {
+
+        WriteByte(sensorTemp, READ_ROM);    // Read ROM
+        for (uint32_t i = 0; i < 8; ++i) {
+            ROM_Code.rom[i] = ReadByte(sensorTemp);
+        }
+    }
+    return ROM_Code;
+}
+
+// temperature is store as 7.4 fixed point format (assuming 12 bit conversion)
+void displayTemperature(Serial& s) {
+    DoConversion();
+    uint32_t temp = GetTemperature();
+    float f = (temp & 0x0F) * 0.0625;    // calculate .4 part
+    f += (temp >> 4);    // add 7.0 part to it
+    s.printf("Temp is %2.1fC\n\r", f);    // display in 2.1 format
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,43 @@
 #include "mbed.h"
+#include <stdint.h>
+#include "DS18B20.h"
+
+DigitalInOut sensorTemp(D3);     // sensor connected to pin 5
+AnalogIn sensorLiquid(A0);
+
+PwmOut mypwm(PWM_OUT);
+
+Ticker timer;                // used for our microsec timing
+Serial pc(USBTX, USBRX);     // serial comms over usb back to console
 
 int main() {
+    pc.baud(115200);
 
+
+    mypwm.period_ms(10);
+    mypwm.pulsewidth_ms(1);
+
+    printf("pwm set to %.2f %%\n", mypwm.read() * 100);
+
+
+    pc.printf("\n\r=====================================================\n\r");
+    pc.printf("DS18B20 Configuration\n\r");
+    sensorTemp.mode(PullUp);
+
+    ROM_Code_t ROM_Code = ReadROM();
+    pc.printf("Family code: 0x%X\n\r", ROM_Code.BYTES.familyCode);
+    pc.printf("Serial Number: ");
+    for (uint32_t i = 6; i != 0; --i) {
+        pc.printf("%02X%s", ROM_Code.BYTES.serialNo[i-1], (i != 1)?":":"\r\n");
+    }
+    pc.printf("CRC: 0x%X\r\n", ROM_Code.BYTES.Crc);
+
+    pc.printf("\n\rRunning temperature conversion...\n\r");
+    while (1) {
+        displayTemperature(pc);
+        wait(0.5);
+        float val = sensorLiquid.read();
+        pc.printf("Liquid level: %f\r\n\r\n", val);
+        wait(1);
+    }
 }
